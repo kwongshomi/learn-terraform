34a9723340275810de0ce29d45d475506b691658
Ken Wong <ken.wong@shomi.com>
2016-05-19 16:32:09 -0400

    Add some comments to Terraform file

--- example-chef.tf
+++ example-chef.tf
@@ -1,9 +1,11 @@
+# Keys abstracted out to separate variable files
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region = "${var.region}"
 }
 
+# The main resource to setup an instance
 resource "aws_instance" "example" {
     ami = "${lookup(var.amis, var.region)}"
     instance_type = "t2.micro"
@@ -15,10 +17,13 @@ resource "aws_instance" "example" {
       Name = "${var.node_name}"
     }
     
+    # Just an example of a local-exec.  This writes to the directory where Terraform is started.
     provisioner "local-exec" {
       command = "echo ${aws_instance.example.public_ip} > file.txt"
     }
 
+    # This provisioner copies a file from the Terraform run directory to the remote instance.
+    # Need the connection section to SSH in as a user other than the default of root.
     provisioner "file" {
       connection {
         user = "centos"
@@ -28,6 +33,7 @@ resource "aws_instance" "example" {
       destination = "/tmp/edithosts.sh"
     }
 
+    # This provisioner runs the script that is copied to the remote server by the previous step.
     provisioner "remote-exec" {
       connection {
         user = "centos"
@@ -36,6 +42,7 @@ resource "aws_instance" "example" {
       inline = ["sudo chmod +x /tmp/edithosts.sh; sudo su -c '/tmp/edithosts.sh'"]
     }
 
+    # This is the Chef provisioner.  It bootstraps the instance and runs the specified recipe/cookbook.
     provisioner "chef" {
         connection {
           user = "centos"
@@ -54,10 +61,12 @@ resource "aws_instance" "example" {
     }
 }
 
+# This allocates an EIP and attaches it to the instance
 resource "aws_eip" "ip" {
     instance = "${aws_instance.example.id}"
 }
 
+# This just prints the EIP to stdout
 output "ip" {
     value = "${aws_eip.ip.public_ip}"
 }
